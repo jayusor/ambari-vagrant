7089ee5141c7c12ccbc6ebfb5418e154b2ae626d
jayusor <jayusor@gmail.com>
2014-06-02 18:28:04 +0200

    export hadoop variable

--- centos6.5/bootstrap.sh
+++ centos6.5/bootstrap.sh
@@ -14,7 +14,14 @@ sudo R -e 'update.packages(repo="http://cran.rstudio.com",ask=F)'
 sudo R -e 'install.packages(c("data.table","devtools"),repo="http://cran.rstudio.com")'
 
 #sudo HADOOP_CMD=/usr/bin/hadoop
-#sudo export HADOOP_CMD 
+#sudo export 
+echo 'HADOOP_CMD=/usr/bin/hadoop' >>  /etc/environment
+echo 'HADOOP_STREAMING=/usr/lib/hadoop-mapreduce/hadoop-streaming.jar' >>  /etc/environment
+
+
+
+
+
 
 #sudo R -e 'options(repos=structure(c(CRAN="http://cran.rstudio.com")));devtools::install_github("rmr2", "RevolutionAnalytics",subdir="pkg")'
 #sudo R -e 'options(repos=structure(c(CRAN="http://cran.rstudio.com")));devtools::install_github("rhdfs", "RevolutionAnalytics",subdir="pkg")'
