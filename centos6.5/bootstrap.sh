5f1c5bc0b3aa9b96b9d8d8cd62d6a14358cd3720
Jorge Ayuso <jayusor@gmail.com>
2014-06-08 22:45:48 +0200

    cambios

--- centos6.5/bootstrap.sh
+++ centos6.5/bootstrap.sh
@@ -7,22 +7,33 @@ service ntpd start
 service iptables stop
 mkdir -p /root/.ssh; chmod 600 /root/.ssh; cp /home/vagrant/.ssh/authorized_keys /root/.ssh/
 
-#sudo yum update -y 
-sudo yum install java-1.7.0-open* -y 
-sudo yum install R curl-devel wget -y
-R CMD javareconf
+sudo yum update -y
+#sudo yum install java-1.7.0-open* -y
+ 
+sudo yum install java-1.7.0-open*  wget curl-devel readline-devel texlive texlive-latex texlive-texmf-fonts cairo-devel libtiff-devel libjpeg-devel -y 
+sudo yum groupinstall 'Development Tools' -y
+
+cd /opt/
+sudo wget http://cran.r-project.org/src/base/R-3/R-3.1.0.tar.gz
+sudo tar -xzvf R-3.1.0.tar.gz 
+cd /opt/R-3.1.0
+sudo ./configure --enable-R-shlib  --with-x=no
+sudo make
+sudo make install
+
+
+
+#sudo yum install R curl-devel wget -y
+#R CMD javareconf
 
 sudo R -e 'update.packages(repo="http://cran.rstudio.com",ask=F)' 
 sudo R -e 'install.packages(c("data.table","devtools","rJava"),repo="http://cran.rstudio.com")'
 
-#sudo HADOOP_CMD=/usr/bin/hadoop
-#sudo export 
 echo 'HADOOP_CMD=/usr/bin/hadoop' >>  /etc/environment
 echo 'HADOOP_STREAMING=/usr/lib/hadoop-mapreduce/hadoop-streaming.jar' >>  /etc/environment
 
 
-
 #sudo R -e 'options(repos=structure(c(CRAN="http://cran.rstudio.com")));devtools::install_github("rmr2", "RevolutionAnalytics",subdir="pkg")'
-#sudo R -e 'options(repos=structure(c(CRAN="http://cran.rstudio.com")));devtools::install_github("rhdfs", "RevolutionAnalytics",subdir="pkg")devtools::install_github("rhdfs", "RevolutionAnalytics",subdir="pkg")devtools::install_github("rhdfs", "RevolutionAnalytics",subdir="pkg")'
+#sudo R -e 'options(repos=structure(c(CRAN="http://cran.rstudio.com")));devtools::install_github("rhdfs", "RevolutionAnalytics",subdir="pkg")'
 #sudo R -e 'options(repos=structure(c(CRAN="http://cran.rstudio.com")));devtools::install_github("rhbase", "RevolutionAnalytics",subdir="pkg")'
 #sudo R -e 'options(repos=structure(c(CRAN="http://cran.rstudio.com")));devtools::install_github("plyrmr", "RevolutionAnalytics",subdir="pkg")'
--- centos6.5/rstudio.sh
+++ centos6.5/rstudio.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+
+sudo yum install libcrypto.so.6  openssl098e-0.9.8e  gcc41-libgfortran-4.1.2 pango-1.28.1  -y
+sudo yum install  atlas blas compat-libgfortran-41  libgfortran cups dbus desktop-file-utils libXmu libicu poppler-utils portreserve tcl tk tmpwatch xdg-utils -y
+
+
 wget http://download2.rstudio.org/rstudio-server-0.98.507-x86_64.rpm
-sudo yum install --nogpgcheck rstudio-server-0.98.507-x86_64.rpm -y
+sudo rpm -Uvh --nodeps rstudio-server-0.98.507-x86_64.rpm
 sudo rstudio-server start
 sudo rm rstudio*.rpm
 
